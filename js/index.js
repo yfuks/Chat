1d7e6d6d831e7225254837a44330c5417ddecc5a
Yoann FUKS <yfuks@e3r3p12.42.fr>
2015-07-29 19:17:44 +0200

    first step for serv

---
+++ js/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/../'));
+app.get('/', function(req, res){
+	var path = require("path");
+	res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
