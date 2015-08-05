b0916c1e23c3f6fac6fdf6dab768430ee2d5f149
Yoann FUKS <yfuks@e3r6p11.42.fr>
2015-08-05 16:34:16 +0200

    well formated comment

--- js/client.js
+++ js/client.js
@@ -6,7 +6,7 @@ Create the 29 jully 2015 by Yoann Fuks
 
 var me = "";
 
-/* this function is for automatic connection when the user login */
+/* this function is for automatic connection when the user have a cookie */
 socket.on('connect', function() {
   if ($.cookie('chat'))
     socket.emit('newusr', $.cookie('chat'));
@@ -23,12 +23,13 @@ $('#button').click(function(){
 
 /* This function print an error if the serv send an error to the client*/
 socket.on('errorlog', function(){
-$('.error-msg').append("<h6>Login allready taken</h6>");
+  $('.error-msg').append("<h6>Login allready taken</h6>");
 });
 
-/* */
+/* When a error with the command arrive */
 socket.on('errorcmd', function(msg){
-$('.chat-body').append("<div class=\"cell\"><div class=\"msg\">" + msg + "</div></div>");
+  $('.chat-body').append("<div class=\"cell\"><div class=\"msg\">" + msg + "</div></div>");
+
 /* Scroll to the botton of the div */
   $('.chat-body').scrollTop($('.chat-body')[0].scrollHeight);
 });
@@ -48,7 +49,8 @@ socket.on('login', function(usr){
       me = usr;
       $('.container1').hide();
     }
-    // If the user isn't allready print
+    
+    /* If the user isn't allready printed */
   	if (!document.getElementById(usr))
     {
       if ($.cookie('chat') === usr || me == usr)
@@ -58,7 +60,7 @@ socket.on('login', function(usr){
     }
 });
 
-/* This function send a new message when the button is click */
+/* This function send a new message when the button is clicked */
 $('.chat-send').click(function(){
 if ($('#mssg').val() == '')
 	return false;
@@ -67,7 +69,7 @@ if ($('#mssg').val() == '')
   $('#mssg').val('');
 });
 
-/* This function handle exit */
+/* This function handle logout */
 $('.exit').click(function(){
   socket.emit('usrdeco', me);
   if ($.cookie('chat'))
@@ -107,18 +109,22 @@ $('#lg_username').keyup(function (e) {
 
 /* This function handle when a new msg arrive from the serv */
 socket.on('newmsg', function(msg){
+
+  /* This part is for the time */
   var d = new Date();
-  d.setTime( d.getTime() - d.getTimezoneOffset()*60*1000);
+  d.setTime(d.getTime() - d.getTimezoneOffset() * 60 * 1000);
   var h = d.getUTCHours();
   var m = d.getUTCMinutes();
   if (m.toString().length == 1)
     m = '0' + m.toString();
+
   $('.chat-body').append("<div class=\"cell\"><div class=\"time\">" + h + ":" + m + "</div><div class=\"msg\">" + msg + "</div></div>");
+  
   /* Scroll to the botton of the div */
   $('.chat-body').scrollTop($('.chat-body')[0].scrollHeight);
 });
 
 /* This function remove a user from the list when the client receive the usrdeco */
 socket.on('usrdeco', function(usr){
-$('#' + usr).remove();
+  $('#' + usr).remove();
 });
